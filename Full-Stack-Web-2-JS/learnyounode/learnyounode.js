ba98e83daf75e4316119d6d3a10f4c21eccbbd09
Jayce Azua <jayce.azua@gmail.com>
2018-01-09 01:47:27 -0800

    learnyounode exercise 5 complete

--- Full-Stack-Web-2-JS/learnyounode/learnyounode.js
+++ Full-Stack-Web-2-JS/learnyounode/learnyounode.js
@@ -36,6 +36,36 @@
 
 /***********************************/
 // Learn you Node 5
+// requiring the filesystem module
+/*
+var fs = require('fs');
+// requiring path module
+var path = require('path');
+// /path/to/dir
+var dir = process.argv[2];
+// file extension
+var fileExt = "." + process.argv[3];
 
+fs.readdir(dir, function(err, lists) {
+    // error-first callback
+    if (err) throw err;
 
+	// long way
 
+    for (var i = 0; i < lists.length; i++) {
+        if (path.extname(lists[i]) === fileExt) {
+        	console.log(lists[i]);
+        }
+    }
+
+    // .forEach()
+    lists.forEach(function(list) {
+    	if (path.extname(list) === fileExt) {
+    		console.log(list)
+    	}
+    });
+});
+*/
+
+/***********************************/
+// Learn you Node 6
