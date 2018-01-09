b8f0d043d451fa96241e76fc3307d8415cbe0826
Jayce Azua <jayce.azua@gmail.com>
2018-01-09 01:12:21 -0800

    learn you node

--- Full-Stack-Web-2-JS/learnyounode/learnyounode.js
+++ Full-Stack-Web-2-JS/learnyounode/learnyounode.js
@@ -14,12 +14,28 @@
 /***********************************/
 // Learn you Node 3
 
-var fs = require('fs')
-var filePath = process.argv[2];
-// synchronous === blocking
-console.log(fs.readFileSync(filePath, 'utf8').split('\n').length -1)
+// var fs = require('fs')
+// var filePath = process.argv[2];
+// // synchronous === blocking
+// console.log(fs.readFileSync(filePath, 'utf8').split('\n').length -1)
 
 
+/***********************************/
+// Learn you Node 4
+
+// var fs = require('fs');
+// var filePath = process.argv[2];
+
+// fs.readFile(filePath, 'utf8', function(err, data) {
+
+// 	if (err) throw err;
+
+// 	console.log(data.split('\n').length - 1);
+
+// })
+
+/***********************************/
+// Learn you Node 5
 
 
 
