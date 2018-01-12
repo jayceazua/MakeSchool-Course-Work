cc495cac31b59f6385def27005919f9c0edad17a
Jayce Azua <jayce.azua@gmail.com>
2018-01-12 15:39:04 -0800

    shopping cart

---
+++ Full-Stack-Web-2-JS/classWork-web-2-cart/app.js
@@ -0,0 +1,64 @@
+var cart = [];
+
+// function to save cart
+var saveCart = function (cart) {
+	saved = [];
+	return saved.concat(cart);
+
+}
+// function to return the total cost
+function returnTotalCost () {
+	var total = 0;
+	for (var i = 0; i < cart.length; i++) {
+		// Should total price be item price be price * qty
+		total += (cart[i].price * cart[i].quantity)
+	}	
+	return total;
+}
+
+
+// addItem 
+function addItem(name, description, quantity, price, id) {
+	cart.push(new NewItem(name, description, quantity, price, id))
+}
+
+// addOneItem <-- Increases qty of item id +1
+function addOneItem (item) {
+	return item.quantity++
+}
+// removeOneItem <-- Decreases qty of item id -1
+function removeOneItem (item) {
+	return item.quantity--
+}
+// clearCart <-- removes all items from cart
+function clearCart () {
+		cart = [];
+}
+// getCartCount <-- returns total count of all items (sum of qty)
+function getCartCount() {
+	var total = 0;
+	for(var i = 0; i < cart.length; i++) {
+		total += cart[i].quantity
+	}
+	return total
+}
+
+
+
+
+// Define a new item
+function NewItem(name, description, quantity, price, id) {
+	this.name = name;
+	this.description = description;
+	this.quantity = quantity;
+	this.price = price;
+	this.id = id
+}
+
+
+// mock item data
+var rubberDuck = addItem('Rubber Duck', 'A rubber duck to float on your bathtub.', 3, 5.75, 'RD0001');
+var tigerToy = addItem('Tiger Stuffed Animal', 'A stuffed animal that looks like tiger.', 2, 10.99, 'TT0002');
+
+
+
