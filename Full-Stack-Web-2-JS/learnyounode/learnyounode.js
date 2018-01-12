66c59034a1ccb6d7b300a7441c4e5994b079316c
Jayce Azua <jayce.azua@gmail.com>
2018-01-12 02:10:35 -0800

    finished learnyounode

--- Full-Stack-Web-2-JS/learnyounode/learnyounode.js
+++ Full-Stack-Web-2-JS/learnyounode/learnyounode.js
@@ -205,5 +205,60 @@ server.listen(port)
 */
 
 /***********************************/
-/* Learn you Node 12 */
+/* Learn you Node 12 
 
+var http = require("http");
+var port = Number(process.argv[2]);
+ // external npm packages
+var map = require("through2-map");
+
+ var server = http.createServer(function(req, res) {
+    if(req.method != "POST")
+        return res.send("Send me a POST\n");
+
+    req.pipe(map(function(data) {
+        return data.toString().toUpperCase();
+
+    })).pipe(res)
+ })
+
+server.listen(port)
+*/
+
+/***********************************/
+/* Learn you Node 13 */
+
+var http = require('http');
+var url = require('url');
+
+function parsetime (time) {
+  return {
+    hour: time.getHours(),
+    minute: time.getMinutes(),
+    second: time.getSeconds()
+  }
+}
+
+function unixtime (time) {
+  return { unixtime : time.getTime() }
+}
+
+var server = http.createServer(function(req, res) {
+  var parsedUrl = url.parse(req.url, true);
+  var time = new Date(parsedUrl.query.iso);
+  var result
+
+  if (/^\/api\/parsetime/.test(req.url))
+    result = parsetime(time);
+  else if (/^\/api\/unixtime/.test(req.url))
+    result = unixtime(time);
+
+  if (result) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(result));
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+});
+server.listen(Number(process.argv[2]));
