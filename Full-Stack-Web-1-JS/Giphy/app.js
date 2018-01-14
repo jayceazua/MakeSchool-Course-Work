0bc08851e0f31fcb3bb7ee95c3d273b4c679f123
Jayce Azua <jayce.azua@gmail.com>
2018-01-14 05:32:14 -0800

    styling the giphy

--- Full-Stack-Web-1-JS/Giphy/app.js
+++ Full-Stack-Web-1-JS/Giphy/app.js
@@ -6,7 +6,7 @@ var app = express();
 
 var exphbs = require('express-handlebars')
 
-
+/*
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
@@ -16,15 +16,16 @@ app.get('/', function (req, res) {
     res.render('home', {gifs: response.data})
   });
 });
+*/
 
-/*
-** We are using Node's native http module **
+/* We are using Node's native http module */
+
 app.get('/', function (req, res) {
   var queryString = req.query.term;
   // ENCODE THE QUERY STRING TO REMOVE WHITE SPACES AND RESTRICTED CHARACTERS
   var term = encodeURIComponent(queryString);
   // PUT THE SEARCH TERM INTO THE GIPHY API SEARCH URL
-  var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
+  var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC';
 
   http.get(url, function(response) {
     // SET ENCODING OF RESPONSE TO UTF8
@@ -32,29 +33,33 @@ app.get('/', function (req, res) {
 
     var body = '';
 
+    // listens for the event of the data buffer and stream
     response.on('data', function(d) {
-
       // CONTINUOUSLY UPDATE STREAM WITH DATA FROM GIPHY
       body += d;
     });
 
+    // once it gets data it parses it into json 
     response.on('end', function() {
       // WHEN DATA IS FULLY RECEIVED PARSE INTO JSON
       var parsed = JSON.parse(body);
       // RENDER THE HOME TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
       res.render('home', {gifs: parsed.data})
     });
+
   });
 })
-*/
 
 
+// send the data to this route
 app.get('/hello-gif', function(request, response) {
 	var gifUrl = "https://media.giphy.com/media/B77Yj3aZYZjmE/giphy.gif"
 	// using the .render() is for a view template
 	response.render('hello-gif', {gifUrl});
-})
+});
 
+
+// bullshit
 app.get('/greetings/:name', function(request, response) {
     var name = request.params.name;
     response.render('greetings', {name});
