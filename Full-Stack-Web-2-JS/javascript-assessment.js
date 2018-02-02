cd8b3f8392554cba2f3a046ad85e068a2db53e85
Jayce Azua <jayce.azua@gmail.com>
2018-02-01 20:05:51 -0800

    i will be back later

--- Full-Stack-Web-2-JS/javascript-assessment.js
+++ Full-Stack-Web-2-JS/javascript-assessment.js
@@ -8,21 +8,31 @@ function splitTextData (textData) {
     return tempData;
 }
 
+
 fetch('MOCK_DATA.csv').then(function(res) {
     return res.text()
 }).then(function(text) {
-    // console.log(text); // all text
-    // get the first line
+
     let textData = text.split('\n');
     let keys = textData.shift().split(',');
-    let tempData = splitTextData(textData);
+    
+    
+    let valueData = splitTextData(textData);
+    
     let tempObj = {};
     
-    console.log(textData)
-
+    console.log()
     
-    console.log(tempData)
-
+    for (var i in keys) {
+        tempObj[keys];
+    }
+    
+    
+    
+    
+    for (var i = 0; i < valueData.length; i++) {
+        console.log(tempObj[keys]);
+    }
     // loop through all items in tempData
         // test the valueForKey should array of id, first, last, ...
             // log key and value  id, 1   first_name, Alec ... 
