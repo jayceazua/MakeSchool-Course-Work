1bc62aefb8f90f4bad339ff1152eaeda7457c398
Jayce Azua <jayce.azua@gmail.com>
2018-02-23 16:27:25 -0800

    react tutorial

---
+++ React-Web-5/js_practice.js
@@ -0,0 +1,16 @@
+// Classes are special functions
+// use a class declaration
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width
+  }
+}
+
+// Hoisting
+// function and class declarations major difference
+//  function declarations are hoisted and class are not
+// you need to declare the class and THEN access it
+
+// class expressions - another way to define a class
+// they can be named or un-named
