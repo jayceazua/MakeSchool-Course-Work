1f9d2d53b7f9bcbc2a1d0b5c1239541ed1be21e5
Jayce Azua <jayce.azua@gmail.com>
2018-02-26 02:19:41 -0800

    fixed webpack bugs

--- React-Web-5/React-Boilerplate/app/app.jsx
+++ React-Web-5/React-Boilerplate/app/app.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+var objOne = {
+  name: 'Jayce',
+  location: 'San Francisco'
+}
+
+// object spread just like array spread ...
+var objTwo = {
+  age: 25,
+  ...objOne
+}
+
+console.log(objTwo)
 
 ReactDOM.render(
   <h1>React boilerplate app !</h1>,
